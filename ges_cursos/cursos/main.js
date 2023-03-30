d68922309c0be0dd8b547ea6cdb4db2d6799ca1f
juan david <juandavidlopezguisao@gmail.com>
2023-03-30 09:00:37 -0500

    primera subida

---
+++ ges_cursos/cursos/main.js
@@ -0,0 +1,10 @@
+const botonguardar = document.querySelector('.botonguardar')
+const botoncancelar =document.querySelector('.botoncancelar')
+
+botonguardar.addEventListener('click', ()=> {
+    alert('Guardado con exito')
+})
+
+botoncancelar.addEventListener('click', ()=> {
+    alert('Cancelado')
+})
